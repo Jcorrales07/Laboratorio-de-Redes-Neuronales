69b15e3f7e45d39b18b3f13846c58d59ead6cc49
Joe Corrales <corralesjoe07@gmail.com>
2025-09-21 13:27:17 -0600

    parte 4 completo

--- parte 4.py
+++ parte 4.py
@@ -2,180 +2,205 @@ import numpy as np
 import DnnLib
 import json
 
-train_data = np.load('mnist_train.npz')
-test_data = np.load('mnist_test.npz')
+def to_one_hot(labels):
+    one_hot_labels = []
 
-images = train_data['images'].reshape(train_data['images'].shape[0], -1) / 255
-labels = train_data['labels']
+    for label in labels:
+        out = [0] * 10
+        out[label] = 1
+        one_hot_labels.append(out)
 
-test_images = test_data['images'].reshape(test_data['images'].shape[0], -1) / 255
-test_labels = test_data['labels']
+    return np.array(one_hot_labels)
 
-test_one_hot = []
-for label in test_labels:
-    out = [0] * 10
-    out[label] = 1
-    test_one_hot.append(out)
-test_one_hot = np.array(test_one_hot) 
+def load_data(train_path='mnist_train.npz', test_path='mnist_test.npz', scale=255, val_per=0.1):
+    train_data = np.load(train_path)
+    test_data = np.load(test_path)
 
-N = images.shape[0]
-val_size = int(N * 0.1)
+    images = train_data['images'].reshape(train_data['images'].shape[0], -1) / scale
+    labels = train_data['labels']
 
-idx = np.arange(N)
-np.random.shuffle(idx)
+    test_images = test_data['images'].reshape(test_data['images'].shape[0], -1) / scale
+    test_labels = test_data['labels']
 
-val_idx, train_idx = idx[:val_size], idx[val_size:] 
+    # -----------------------------
+    N = images.shape[0]
+    val_size = int(N * val_per)
 
-val_images, val_labels = images[val_idx], labels[val_idx]
-train_images, train_labels = images[train_idx], labels[train_idx]
+    indices = np.arange(N)
+    np.random.shuffle(indices)
+    # -----------------------------
 
-val_one_hot = []
-for label in val_labels:
-    out = [0] * 10
-    out[label] = 1
-    val_one_hot.append(out)
-val_one_hot = np.array(val_one_hot) 
+    val_indices, train_indices = indices[:val_size], indices[val_size:]
 
-print(f"\n valuation shape: {val_images.shape}, train shape {train_images.shape}")
+    val_images, val_labels = np.array(images[val_indices]), np.array(labels[val_indices])
+    train_images, train_labels = np.array(images[train_indices]), np.array(labels[train_indices])
 
-layer1 = DnnLib.DenseLayer(784, 128, DnnLib.ActivationType.RELU)
-layer2 = DnnLib.DenseLayer(128, 10, DnnLib.ActivationType.SOFTMAX)
-
-optimizer = DnnLib.Adam(learning_rate=0.001)
-
-N = train_images.shape[0]
-
-epochs, batch_size = 15, 216
-
-for epoch in range(1, epochs + 1):
-    epoch_loss, epoch_acc = [], []
-    
-    for i in range(0, N, batch_size):
-        batch = []
-        batch_labels = []
+    train_oh = to_one_hot(train_labels)
+    val_oh = to_one_hot(val_labels)
+    test_oh = to_one_hot(test_labels)
         
-        start_idx = i
-        end_idx = min(i + batch_size, N)
+    return (train_images, train_labels, train_oh), (val_images, val_labels, val_oh), (test_images, test_labels, test_oh)
+
+def build_model():
+    layers = [
+        DnnLib.DenseLayer(784, 128, DnnLib.ActivationType.RELU),
+        DnnLib.DenseLayer(128, 10, DnnLib.ActivationType.SOFTMAX)
+    ]
+
+    optimizer = DnnLib.Adam(learning_rate=0.001)
+    return layers, optimizer
+
+def forward(layers, batch):
+    out = batch
+    for layer in layers:
+        out = layer.forward(out)
+    return out
+
+def backward(layers, grad):
+    for layer in reversed(layers):
+        grad = layer.backward(grad)
         
-        batch = train_images[start_idx:end_idx]
-        batch_labels = train_labels[start_idx:end_idx]
-
-        one_hot_classes = []
-
-        for i in batch_labels:
-            out = [0] * 10
-            out[i] = 1
-            one_hot_classes.append(out)
+def update_params(optimizer, layers):
+    for layer in reversed(layers):
+        optimizer.update(layer)
         
-        one_hot_classes = np.array(one_hot_classes)
+def get_data_batch(n_batch, batch_size, n_samples, train_data):
+    start_idx = n_batch
+    end_idx = min(n_batch + batch_size, n_samples)
 
-        out1 = layer1.forward(batch)
-        out2 = layer2.forward(out1)
+    batch = train_data[0][start_idx:end_idx]
+    batch_labels = train_data[1][start_idx:end_idx]
+    batch_oh = train_data[2][start_idx:end_idx]
+    return batch, batch_labels, batch_oh
 
-        loss = DnnLib.cross_entropy(out2, one_hot_classes)
-        predictions = np.argmax(out2, axis=1)
-        accuracy = (predictions == np.argmax(one_hot_classes, axis=1)).mean()
-        
-        epoch_loss.append(loss)
-        epoch_acc.append(accuracy)
-        
-        scce_grad = DnnLib.cross_entropy_gradient(out2, one_hot_classes)
-        grad1 = layer2.backward(scce_grad)
-        grad2 = layer1.backward(grad1)
+def get_accuracy(output, one_hot_labels):
+    predictions = np.argmax(output, axis=1)
+    correct = np.argmax(one_hot_labels, axis=1)
+    accuracy = (predictions == correct).mean()
+    return accuracy
 
-        optimizer.update(layer2)
-        optimizer.update(layer1)
-
+def get_epoch_metrics(epoch_loss, epoch_acc):
     avg_loss = np.array(epoch_loss).mean()
     avg_acc = np.array(epoch_acc).mean()
-    print(f"Epoch (Training) # {epoch} | Loss : {avg_loss} | Accuracy: {avg_acc}")   
+    return { "avg_loss": avg_loss, "avg_acc": avg_acc }
 
-    out_test1 = layer1.forward(val_images)
-    out_test2 = layer2.forward(out_test1)
+def model_evaluation(layers, data):
+    # Calculo Forward
+    output = forward(layers, data[0])
     
-    val_loss = DnnLib.cross_entropy(out_test2, val_one_hot)
-    val_predictions = np.argmax(out_test2, axis=1)
-    val_accuracy = (val_predictions == np.argmax(val_one_hot, axis=1)).mean()
-    print(f"Epoch (Test)     # {epoch} | Loss : {val_loss} | Accuracy: {(val_accuracy * 100):.2f}%")
+    # Calculo de Perdida
+    loss = DnnLib.cross_entropy(output, data[2])
     
-out_test1 = layer1.forward(test_images)
-out_test2 = layer2.forward(out_test1)
+    # Calculo de Precision
+    accuracy = get_accuracy(output, data[2])
+    return output, loss, accuracy
+
+def save_model(layers, input_space, preprocess, model_acc, filename="mnist_model_"):
+
+    model = {
+        "input_space": input_space,
+        "preprocess": preprocess,
+        "layers": [],
+    }
+
+    for layer in layers:
+        layer_data = {
+            "type" : "dense",
+            "units" : len(layer.bias),
+            "activation" : str(layer.activation_type).split(".")[1].lower(),
+            "W" : layer.weights.T.tolist(),
+            "b" : layer.bias.tolist(),
+        }
+
+        model['layers'].append(layer_data)
+
+    filename = f"{filename}{model_acc:.3f}.json"
+
+    with open(filename, "w") as f:
+        json.dump(model, f)
+
+    print(f'Model saved as {filename}')
     
-test_loss = DnnLib.cross_entropy(out_test2, test_one_hot)
-test_predictions = np.argmax(out_test2, axis=1)
-test_accuracy = (test_predictions == np.argmax(test_one_hot, axis=1)).mean()
+def load_model(filename, test_data):
+    with open(filename, 'r') as f:
+        model_loaded = json.load(f)
 
-print("Model test with mnist_test.npz dataset")
-print(f"Model Test | Accuracy: {(test_accuracy * 100):.2f}%")
+    loaded_layers = model_loaded['layers']
+    scale = model_loaded['preprocess']['scale']
 
-# 5. Guardar el modelo
-    # Guardar el modelo en un archivo json con el formato del ing
+    layers = []
 
-input_shape = [28, 28]
-preprocess = { "scale": 255.0 }
+    for layer in loaded_layers:
+        activation = getattr(DnnLib.ActivationType, layer['activation'].upper())
 
-layer1_data = {
-    "type": "dense",
-    "units": len(layer1.bias),
-    "activation": str(layer1.activation_type).split(".")[1].lower(),
-    "W": layer1.weights.T.tolist(),
-    "b": layer1.bias.tolist()
-}
+        W = np.array(layer['W'])
+        b = np.array(layer['b'])
 
-layer2_data = {
-    "type": "dense",
-    "units": len(layer2.bias),
-    "activation": str(layer2.activation_type).split(".")[1].lower(),
-    "W": layer2.weights.T.tolist(),
-    "b": layer2.bias.tolist()
-}
+        inputs = W.shape[0]
+        neurons = W.shape[1]
 
-model = {
-    "input_space": input_shape,
-    "preprocess": preprocess,
-    "layers": [
-        layer1_data,
-        layer2_data
-    ]
-}
+        new_layer = DnnLib.DenseLayer(inputs, neurons, activation)
 
-with open('mnist_model_v1.json', 'w') as f:
-    json.dump(model, f)
+        new_layer.weights = W.T
+        new_layer.bias = b
+
+        layers.append(new_layer)
+
+    _, _, test_acc = model_evaluation(layers, test_data)
+    print('Model loaded correctly')
+    print(f"Model Test | Accuracy: {(test_acc * 100):.2f}%")
+
+    return layers
+
+def train_model(layers, n_samples, train_data, val_data, epochs=15, batch_size=216):
+    print("Starting model training...\n")
+    print(f"With Epochs: {epochs} Batch size: {batch_size} \n")
+    print("Epochs results: \n")
     
-    
-with open('mnist_mlp_pretty.json', 'r') as f:
-    model_loaded = json.load(f)
+    for epoch in range(1, epochs + 1):
+        epoch_loss, epoch_acc = [], []
+        
+        for n_batch in range(0, n_samples, batch_size):
+            
+            # Calculando el batch
+            batch, batch_labels, batch_oh = get_data_batch(n_batch, batch_size, n_samples, train_data)
+
+            # Calculo Forward
+            output = forward(layers, batch)
+
+            # Calculo de Perdida 
+            loss = DnnLib.cross_entropy(output, batch_oh)
+            
+            # Calculo de Precision
+            accuracy = get_accuracy(output, batch_oh)
+            
+            epoch_loss.append(loss)
+            epoch_acc.append(accuracy)
+            
+            # Calculo de gradiente SCCE
+            scce_gradient = DnnLib.cross_entropy_gradient(output, batch_oh)
+
+            # Calculo Backward
+            gradient = backward(layers, scce_gradient)
+
+            # Actualizacion de parametros
+            update_params(optimizer, layers)
+
+        e_m = get_epoch_metrics(epoch_loss, epoch_acc)
+        print(f"Epoch (Training) # {epoch} | Loss : {e_m['avg_loss']:.2f} | Accuracy: {(e_m['avg_acc'] * 100):.2f}%")
+
+        _, val_loss, val_acc = model_evaluation(layers, val_data)
+        print(f"Epoch (Val)      # {epoch} | Loss : {val_loss:.2f} | Accuracy: {(val_acc * 100):.2f}%")
 
 
-loaded_layers = model_loaded['layers']
-scale = model_loaded['preprocess']['scale']
+train_data, val_data, test_data = load_data()
 
-layers = []
-for layer in loaded_layers:
-    activation = getattr(DnnLib.ActivationType, layer['activation'].upper())
+layers, optimizer = build_model()
 
-    W = np.array(layer['W'])
-    b = np.array(layer['b'])
+train_model(layers, train_data[0].shape[0], train_data, val_data)
 
-    inputs = W.shape[0]
-    neurons = W.shape[1]
+_, _, test_acc = model_evaluation(layers, test_data)
 
-    new_layer = DnnLib.DenseLayer(inputs, neurons, activation)
+print(f"Model Test | Accuracy: {(test_acc * 100):.2f}%")
 
-    print(new_layer.weights.shape)
-    print(new_layer.bias.shape)
-    
-    new_layer.weights = W.T
-    new_layer.bias = b
-
-    layers.append(new_layer)
-
-out_test1 = layers[0].forward(test_images)
-out_test2 = layers[1].forward(out_test1)
-    
-test_loss = DnnLib.cross_entropy(out_test2, test_one_hot)
-test_predictions = np.argmax(out_test2, axis=1)
-test_accuracy = (test_predictions == np.argmax(test_one_hot, axis=1)).mean()
-
-print("Model test with mnist_test.npz dataset")
-print(f"Model Test | Accuracy: {(test_accuracy * 100):.2f}%")
+save_model(layers, [28, 28], {"scale": 255.0}, test_acc)
